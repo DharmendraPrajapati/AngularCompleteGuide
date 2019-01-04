4147c3efa2af0a1cd3ec885ec638d29faaca3dae
Dharmendra Prajapati <dharmendra_prajapati@epam.com>
2019-01-04 13:31:48 +0530

    Add code for string event and two way binding with basics

--- AngularComplete/src/app/server/server.component.ts
+++ AngularComplete/src/app/server/server.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-    selector:'app-server',
+    // selector:'[app-server]', select by attribute
+    // selector:'.app-server', select by class
+    selector:'app-server', // select by element
     templateUrl: './server.component.html'
 })
 export class ServerComponent implements OnInit {
+    serverId :number = 10;
+    serverStatus : string = 'Offline';
     ngOnInit() : void {
         console.log('check');
     }
--- AngularComplete/src/app/servers/servers.component.ts
+++ AngularComplete/src/app/servers/servers.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+allowNewServer : boolean = false;
+serverCreationStatus: string = 'No Server Created';
+serverName: string = 'First Name';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was Created and the server name is '+ this.serverName;
+  }
+
+  onServerUpdate(event:Event){
+    /*
+    this.serverName = event.target.value; 
+    So this line will give the error because typescript so not know that the what kind of element this event will give.
+    So for this we need to cast this event's target type as HTMLInputElement. 
+    Like we need to tell tyepescript that this event's target will be input element type.
+
+    */
+    this.serverName = (<HTMLInputElement>event.target).value; 
+  }
+  onServerNameUpdate(){
+    this.serverName="Click Button Server";
+  }
 }
