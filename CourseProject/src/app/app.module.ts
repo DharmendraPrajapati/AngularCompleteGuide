eb84ba91c3b1d01d4d5091c3e79644ea044e4637
Dharmendra Prajapati <dharmendra_prajapati@epam.com>
2019-01-08 19:33:29 +0530

    Add course Project and build basic things like header

--- CourseProject/src/app/app.module.ts
+++ CourseProject/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesItemComponent,
+    RecipesDetailsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
--- CourseProject/src/app/header/header.component.ts
+++ CourseProject/src/app/header/header.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '../../../node_modules/@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector:'app-header',
-    templateUrl:'./header.component.html',
-    styles:[]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
-    ngOnInit(): void {
-        console.log('hit !!');
-    }
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
