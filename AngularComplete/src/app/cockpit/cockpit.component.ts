6f081042aa7fc3dc6baaed7ab941626527d99228
Dharmendra Prajapati <dharmendra_prajapati@epam.com>
2019-01-11 18:45:04 +0530

    Add code

---
+++ AngularComplete/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+@Output() serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
+@Output() blueprintCreated = new EventEmitter<{serverName:string,serverContent:string}>();
+
+  constructor() { }
+  ngOnInit() { }
+
+  newServerName = '';
+  newServerContent = '';
+  
+  onAddServer() {
+    this.serverCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent});
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({serverName:this.newServerName,serverContent:this.newServerContent});
+  }
+}
---
+++ AngularComplete/src/app/server-element/server-element.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+ //* @input 
+  @Input() element: {type:string,name:string,content:string};
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+/* 
+  * @input- As if we do not add anyu decorator to element, we can not use this element outside of the class because class property by default 
+    can be use inside the class like private. So if you want to expose this property to outside we need to have decorator. Decorator is not only for class.
+    So we will use @input decorator to tell angular that this property will be expose to outside so outside compenent can assign a value to it.
+  * If we do not want to bind element property name and we want to bind propertywith some aliases we can use like this 
+    @input('srvElement') element: {type:string,name:string,content:string};  and use this alias 'srvElement' in html like [srvElement] inplace of [element].
+  * Same aliases will work for output decorator as well in same manner.
+  * View Encapsulation - View encapsulation ih angular is like we have right now css which says that text inside <p> should be in blue 
+    color.But isn't that strange here on this CSS definition which just say paragraph. So it should affect all paragraphs or a whole app.
+    Now we might see that it's in the app components. As files so clearly it only belongs to this template.But Because the way C S S works it doesn't really care in which C S S fall.
+    You define a rule in CSS. It simply is applied to the whole document normally.So this actually is a behavior enforced by angular which is not the default behavior of the browser.
+    Have the goal of encapsulating styles for the component they belong to.This again is not a default behavior.
+    Angler gives us this behavior and it's a great behavior because we have that we can make sure that whichever styles we define in us as file will only get applied to the component they belong to.
+*/
